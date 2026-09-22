4ea593f800f6f1b02789c2ee0fae6fd36623bf45
Adrian <thpham24@myseneca.ca>
2026-09-22 01:40:53 +0000

    done lab2a

--- lab2a.py
+++ lab2a.py
@@ -1,13 +1,22 @@
 # Add comments before you do anything else.
 
 #!/usr/bin/env python3
-# Author:
-# Date:
+# Author: Thien Hung Pham
+# Date: 21/09/2026
 # Purpose: Create a variable, check its type and print the variable.
 # Usage: ./lab2a.py
 
 # TO DO 1: Follow the instructions given in README.md file
+x = input("Enter an integer: ")
+print(f"Type of x: {type(x)}")
 
+# Convert the input string to an integer.
+x = int(x)
 
+# Check whether x is greater than or equal to 6.
+if x >= 6:
+    print("x is greater then 6!")
 
-
+# Check whether x is between 4 and 12, inclusive of 4 and exclusive of 12.
+if x >= 4 and x < 12:
+    print("x is greater than or equal to 4 and less than 12!")
